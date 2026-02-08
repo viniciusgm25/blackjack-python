238233e1c518093c8d3563c5b21a203f13f76130
VINICIUS GONCALVES MESQUITA <vinicius.goncalves2503@gmail.com>
2026-02-08 04:37:44 -0300

    Update deck.py

--- src/deck.py
+++ src/deck.py
@@ -2,7 +2,11 @@ import random
 
 class Deck:
     def __init__(self):
-        self.cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] * 4
+        self.cards = (
+            [2, 3, 4, 5, 6, 7, 8, 9] * 4 +
+            [10] * 16 +
+            [11] * 4
+        )
         random.shuffle(self.cards)
 
     def draw_card(self):
